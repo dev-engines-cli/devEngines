a685e021c9ce47dfdd33ab209fb213eb1ace0eb3
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-03-24 21:23:26 -0400

    Shims

---
+++ shims/node.js
@@ -0,0 +1,7 @@
+/**
+ * @file Shim for running Node.js.
+ */
+
+import { runShim } from '../src/shims.js';
+
+await runShim('node');
---
+++ shims/npm.js
@@ -0,0 +1,7 @@
+/**
+ * @file Shim for running npm.
+ */
+
+import { runShim } from '../src/shims.js';
+
+await runShim('npm');
